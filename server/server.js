4cd88aa4f39ad7bff66835a56ca40ebdd2848f38
Kei Takahashi <ktakahashi@minitab.com>
2021-10-28 16:15:07 -0400

    setup express

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8085;
 
-app.use('/', express.static('/home/ktakahaashi/dev/wasm/test'));
+app.use('/', express.static('bin'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
